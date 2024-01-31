f0c37029d885e84cdd7dd86d16538440a1d22a1c
Maicon Souza <maicon_souza09@hotmail.com>
2024-01-30 23:00:36 -0300

    30-01-24 23:00

---
+++ python/aula48.py
@@ -0,0 +1,11 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+list_array = [123, True, 'Luiz Otávio', 1.2, []]
+list_array.insert(1, '23')
+list_array.insert(0, 'Dante')
+print(list_array)
